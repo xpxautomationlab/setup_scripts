714d808a1b828cc7af8699f39c074d5db8530280
Arik Jones <tnypxl00@icloud.com>
2016-08-10 15:31:03 -0500

    Script to install docker on ubuntu 16.04

---
+++ install_docker_ubuntu_1604.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+apt-cache policy docker-engine
+sudo apt-get install -y docker-engine
+
+# Add current user to docker group
+sudo usermod -aG docker $(whoami)
